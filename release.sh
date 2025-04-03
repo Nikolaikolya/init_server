f864ab2e7059b26116b816f10bacb68a0fe361fa
Nikolaikolya <starcrafters@ya.ru>
2025-04-03 15:42:26 +0700

    add new release

---
+++ release.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Сборка проекта
+echo "Сборка проекта..."
+cargo build --release
+
+# Перемещение исполняемого файла в каталог release
+mv target/release/init_server release/
+
+# Очистка
+cargo clean
+echo "Сборка завершена!"
